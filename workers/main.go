3f0d8a2fe29fea72a083f7b9b5a858509e5197e5
Jusong Chen <jusong.chen@gmail.com>
2016-07-08 18:13:06 -0700

    add comment to worker example

--- workers/main.go
+++ workers/main.go
@@ -7,41 +7,54 @@ import (
 	"time"
 )
 
-func worker(workerID int, tasksCh <-chan int, wg *sync.WaitGroup) {
+type Task int
+
+func worker(workerID int, tasksChannel <-chan Task, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for {
-		task, ok := <-tasksCh
+		task, ok := <-tasksChannel
 		if !ok {
+			//no job left on the tasksChannel
 			return
 		}
-		//		d := time.Duration(task) * time.Millisecond
-		d := time.Duration(rand.Float64()*2) * time.Second
+
 		t := time.Now()
+
+		//sleep for random period to simulate task processing
+		d := time.Duration(rand.Float64()*2) * time.Second
 		time.Sleep(d)
+
 		fmt.Printf("Woker #%d:completed task %d in %s\n", workerID, task, time.Since(t))
 	}
 }
 
-func pool(wg *sync.WaitGroup, workers, tasks int) {
-	tasksCh := make(chan int)
+func pool(wg *sync.WaitGroup, numWorkers, numTasks int) {
 
-	for i := 0; i < workers; i++ {
-		go worker(i, tasksCh, wg)
+	tasksChannel := make(chan Task)
+
+	for i := 0; i < numWorkers; i++ {
+		go worker(i, tasksChannel, wg)
 	}
 
-	for i := 0; i < tasks; i++ {
-		tasksCh <- i
+	//this goroutine generate jobs
+	for i := 0; i < numTasks; i++ {
+		tasksChannel <- Task(i)
 	}
 
-	close(tasksCh)
+	close(tasksChannel)
 }
 
 func main() {
 	var wg sync.WaitGroup
+
 	numWorkers := 8
 	numTasks := 50
 
 	wg.Add(numWorkers)
+
+	//launch a goroutine to generate and execute jobs
 	go pool(&wg, numWorkers, numTasks)
+
+	//wait until all workers returns
 	wg.Wait()
 }
