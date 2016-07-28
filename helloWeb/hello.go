deec8166c2cbc699b7a3c8e4a5e17d5ae9db789e
Jusong Chen <jusong.chen@gmail.com>
2016-07-28 11:28:17 -0700

    helloWeb with error handling

--- helloWeb/hello.go
+++ helloWeb/hello.go
@@ -4,20 +4,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
-
-func greetingHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi %s, how are you doing today?", r.URL.Path[1:])
-}
-
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusBadRequest)
-	fmt.Fprintf(w, "bad request:%s!", r.URL.Path[:])
-}
 
 func main() {
 	http.HandleFunc("/hello/", greetingHandler)
 	http.HandleFunc("/", defaultHandler)
+
 	err := http.ListenAndServe(":8080", nil)
 	log.Fatal(err)
+}
+
+func greetingHandler(w http.ResponseWriter, r *http.Request) {
+	//split url path to two strings using / as delimitor
+	name := strings.SplitN(r.URL.Path[1:], "/", 2)[1]
+	fmt.Fprintf(w, "Hi %s,how are you today?", name)
+}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, fmt.Sprintf("bad request:%s", r.URL.Path[:]), http.StatusBadRequest)
 }
